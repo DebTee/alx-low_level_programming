83817c62b400529b0cca01ea7f89e9f12917f9dd
DebTee <deborahomoniyi2@gmail.com>
2022-06-30 09:00:15 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,16 +4,20 @@
 
 /**
 
-*Description: prints "\"Programming is like building a multilingual puzzle"
+ *  main - Entry point
 
-*/
+ *  Description: prints "\"Programming is like building a multilingual puzzle"
+
+ *  Return: 0
+
+ */
 
 int main(void)
 
- {
+{
 
-  puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-  return(0);
+	return (0);
 
 }
