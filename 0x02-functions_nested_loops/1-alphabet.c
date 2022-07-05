6d969c29023e9fdfc6d8a75c988649fae741eac3
DebTee <deborahomoniyi2@gmail.com>
2022-07-05 13:41:39 -0700

    commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_alphabet - Check description
  * Description: It prints the alphabet in lowercase fallowed by a new line
@@ -11,4 +12,5 @@ void print_alphabet(void)
 	for (i = 'a'; i <= 'z'; i++)
 		_putchar(i);
 	_putchar('\n');
+	return (0)
 }
